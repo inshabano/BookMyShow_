f451441927bde130f4e24e48008964fcd0b1a8dc
Insha Bano <inshabano88158@gmail.com>
2023-12-09 14:28:52 +0530

    model

--- Bookmyshow/src/main/java/com/insha/Bookmyshow/Model/BookingStatus.java
+++
@@ -1,7 +0,0 @@
-package com.insha.Bookmyshow.Model;
-
-public enum BookingStatus {
-    BOOKED,
-    CANCELLED,
-    In_PROGRESS
-}
--- Bookmyshow/src/main/java/com/insha/Bookmyshow/Model/Language.java
+++
@@ -1,6 +0,0 @@
-package com.insha.Bookmyshow.Model;
-
-public enum Language {
-
-        HINDI, ENGLISH, PUNJABI, BENGALI, KANNADA, TELUGU
-}
