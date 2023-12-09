85760bdb0afd19910c32906b8a61a641969f7014
Insha Bano <inshabano88158@gmail.com>
2023-12-09 15:42:00 +0530

    Repo

--- src/main/java/com/insha/Bookmyshow/Repositories/SeatTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insha.Bookmyshow.Repositories;
-
-import com.insha.Bookmyshow.Model.SeatType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeatTypeRepository extends JpaRepository<SeatType, Long> {
-}
