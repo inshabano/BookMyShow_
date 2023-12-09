bb48d61a37ed118073a3c35253689f508c2a2b9d
Insha Bano <inshabano88158@gmail.com>
2023-12-09 14:25:36 +0530

    Repo

--- Bookmyshow/src/main/java/com/insha/Bookmyshow/Repositories/ScreenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insha.Bookmyshow.Repositories;
-
-import com.insha.Bookmyshow.Model.Screen;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScreenRepository extends JpaRepository<Screen , Long> {
-}
--- Bookmyshow/src/main/java/com/insha/Bookmyshow/Repositories/TheatreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insha.Bookmyshow.Repositories;
-
-import com.insha.Bookmyshow.Model.Theatre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TheatreRepository extends JpaRepository<Theatre , Long> {
-}
