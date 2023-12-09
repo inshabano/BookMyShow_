9914c81288d9352d6f305005b1d094cc807919b2
Insha Bano <inshabano88158@gmail.com>
2023-12-09 16:06:33 +0530

    Repo

---
+++ Bookmyshow/src/main/java/com/insha/Bookmyshow/Repositories/ShowSeatTypeRepository.java
@@ -0,0 +1,14 @@
+package com.insha.Bookmyshow.Repositories;
+
+import com.insha.Bookmyshow.Model.Show;
+import com.insha.Bookmyshow.Model.ShowSeatType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ShowSeatTypeRepository extends JpaRepository<ShowSeatType, Long>
+{
+
+    List<ShowSeatType> findAllByShow(Show show);
+}
